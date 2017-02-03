8defcc9203a2dbb6d927e3ecd494d6c5c44baf4b
Sergey Eremin <yeriomin@gmail.com>
2017-02-03 15:00:13 +0300

    openshift modifications

--- src/main/java/com/github/yeriomin/tokendispenser/Server.java
+++ src/main/java/com/github/yeriomin/tokendispenser/Server.java
@@ -4,14 +4,14 @@ import static spark.Spark.*;
 
 public class Server {
 
+    private static final String IP_ADDRESS = System.getenv("OPENSHIFT_DIY_IP") != null ? System.getenv("OPENSHIFT_DIY_IP") : "localhost";
+    private static final int PORT = System.getenv("OPENSHIFT_DIY_PORT") != null ? Integer.parseInt(System.getenv("OPENSHIFT_DIY_PORT")) : 8080;
+
     static PasswordsDb passwords;
 
     public static void main(String[] args) {
-        port(8080);
-//        staticFiles.location("/public");
-        passwords = new PasswordsDb();
-
-//        secure("deploy/keystore.jks", "password", null, null);
+        ipAddress(IP_ADDRESS);
+        port(PORT);
         get("/token/email/:email/gsf-id/:gsf-id", (req, res) -> new TokenResource().handle(req, res));
         get("/token-ac2dm/email/:email", (req, res) -> new TokenAc2dmResource().handle(req, res));
         after((request, response) -> response.type("text/plain"));
