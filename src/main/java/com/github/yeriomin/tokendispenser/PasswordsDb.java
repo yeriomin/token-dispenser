98255a6501b700c2aee7ab775f08397634151f32
Sergey Eremin <yeriomin@gmail.com>
2017-02-03 18:00:28 +0300

    Config moved to Server, spark host and port parametrized properly

--- src/main/java/com/github/yeriomin/tokendispenser/PasswordsDb.java
+++ src/main/java/com/github/yeriomin/tokendispenser/PasswordsDb.java
@@ -9,28 +9,18 @@ import java.util.Properties;
 
 class PasswordsDb {
 
-    static private final String CONFIG_FILE = "/config.properties";
-
     static private final String FIELD_EMAIL = "email";
     static private final String FIELD_PASSWORD = "password";
 
-    static private final String PROPERTY_HOST = "mongodb-host";
-    static private final String PROPERTY_PORT = "mongodb-port";
-    static private final String PROPERTY_USERNAME = "mongodb-username";
-    static private final String PROPERTY_PASSWORD = "mongodb-password";
-    static private final String PROPERTY_DB_STORAGE = "mongodb-databaseNameStorage";
-    static private final String PROPERTY_COLLECTION = "mongodb-collectionName";
-
     private DBCollection collection;
 
-    PasswordsDb() {
-        Properties config = getConfig();
-        String host = config.getProperty(PROPERTY_HOST, "");
-        int port = Integer.parseInt(config.getProperty(PROPERTY_PORT, "0"));
-        String username = config.getProperty(PROPERTY_USERNAME, "");
-        String password = config.getProperty(PROPERTY_PASSWORD, "");
-        String databaseNameStorage = config.getProperty(PROPERTY_DB_STORAGE, "");
-        String collectionName = config.getProperty(PROPERTY_COLLECTION, "");
+    PasswordsDb(Properties config) {
+        String host = config.getProperty(Server.PROPERTY_MONGODB_HOST, "");
+        int port = Integer.parseInt(config.getProperty(Server.PROPERTY_MONGODB_PORT, "0"));
+        String username = config.getProperty(Server.PROPERTY_MONGODB_USERNAME, "");
+        String password = config.getProperty(Server.PROPERTY_MONGODB_PASSWORD, "");
+        String databaseNameStorage = config.getProperty(Server.PROPERTY_MONGODB_DB, "");
+        String collectionName = config.getProperty(Server.PROPERTY_MONGODB_COLLECTION, "");
         Mongo mongo;
         try {
             mongo = new Mongo(host, port);
@@ -61,24 +51,6 @@ class PasswordsDb {
         object.put(FIELD_EMAIL, email);
         object.put(FIELD_PASSWORD, password);
         collection.insert(object);
-    }
-
-    Properties getConfig() {
-        Properties properties = new Properties();
-        try (InputStream input = PasswordsDb.class.getResourceAsStream(CONFIG_FILE)) {
-            properties.load(input);
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
-        String host = System.getenv("OPENSHIFT_MONGODB_DB_HOST");
-        if (null != host && !host.isEmpty()) {
-            properties.put(PROPERTY_HOST, host);
-            properties.put(PROPERTY_PORT, System.getenv("OPENSHIFT_MONGODB_DB_PORT"));
-            properties.put(PROPERTY_USERNAME, System.getenv("OPENSHIFT_MONGODB_DB_USERNAME"));
-            properties.put(PROPERTY_PASSWORD, System.getenv("OPENSHIFT_MONGODB_DB_PASSWORD"));
-            properties.put(PROPERTY_DB_STORAGE, System.getenv("OPENSHIFT_APP_NAME"));
-        }
-        return properties;
     }
 
 }
--- src/main/java/com/github/yeriomin/tokendispenser/Server.java
+++ src/main/java/com/github/yeriomin/tokendispenser/Server.java
@@ -1,19 +1,58 @@
 package com.github.yeriomin.tokendispenser;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 import static spark.Spark.*;
 
 public class Server {
 
-    private static final String IP_ADDRESS = System.getenv("OPENSHIFT_DIY_IP") != null ? System.getenv("OPENSHIFT_DIY_IP") : "localhost";
-    private static final int PORT = System.getenv("OPENSHIFT_DIY_PORT") != null ? Integer.parseInt(System.getenv("OPENSHIFT_DIY_PORT")) : 8080;
+    static private final String CONFIG_FILE = "/config.properties";
+
+    static final String PROPERTY_SPARK_HOST = "spark-host";
+    static final String PROPERTY_SPARK_PORT = "spark-port";
+    static final String PROPERTY_MONGODB_HOST = "mongodb-host";
+    static final String PROPERTY_MONGODB_PORT = "mongodb-port";
+    static final String PROPERTY_MONGODB_USERNAME = "mongodb-username";
+    static final String PROPERTY_MONGODB_PASSWORD = "mongodb-password";
+    static final String PROPERTY_MONGODB_DB = "mongodb-databaseNameStorage";
+    static final String PROPERTY_MONGODB_COLLECTION = "mongodb-collectionName";
 
     static PasswordsDb passwords;
 
     public static void main(String[] args) {
-        ipAddress(IP_ADDRESS);
-        port(PORT);
+        Properties config = getConfig();
+        String host = config.getProperty(PROPERTY_SPARK_HOST, "127.0.0.1");
+        int port = Integer.parseInt(config.getProperty(PROPERTY_SPARK_PORT, "8080"));
+        String hostDiy = System.getenv("OPENSHIFT_DIY_IP");
+        if (null != hostDiy && !hostDiy.isEmpty()) {
+            host = hostDiy;
+            port = Integer.parseInt(System.getenv("OPENSHIFT_DIY_PORT"));
+        }
+        ipAddress(host);
+        port(port);
+        Server.passwords = new PasswordsDb(config);
         get("/token/email/:email/gsf-id/:gsf-id", (req, res) -> new TokenResource().handle(req, res));
         get("/token-ac2dm/email/:email", (req, res) -> new TokenAc2dmResource().handle(req, res));
         after((request, response) -> response.type("text/plain"));
+    }
+
+    static Properties getConfig() {
+        Properties properties = new Properties();
+        try (InputStream input = PasswordsDb.class.getResourceAsStream(CONFIG_FILE)) {
+            properties.load(input);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        String host = System.getenv("OPENSHIFT_MONGODB_DB_HOST");
+        if (null != host && !host.isEmpty()) {
+            properties.put(PROPERTY_MONGODB_HOST, host);
+            properties.put(PROPERTY_MONGODB_PORT, System.getenv("OPENSHIFT_MONGODB_DB_PORT"));
+            properties.put(PROPERTY_MONGODB_USERNAME, System.getenv("OPENSHIFT_MONGODB_DB_USERNAME"));
+            properties.put(PROPERTY_MONGODB_PASSWORD, System.getenv("OPENSHIFT_MONGODB_DB_PASSWORD"));
+            properties.put(PROPERTY_MONGODB_DB, System.getenv("OPENSHIFT_APP_NAME"));
+        }
+        return properties;
     }
 }
