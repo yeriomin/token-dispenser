5b9779a11a0d5e7099bae879128727fe1279b11f
Sergey Eremin <yeriomin@gmail.com>
2017-06-11 17:49:27 +0300

    Using new play-store-api library, random auth errors should be fixed

--- src/main/java/com/github/yeriomin/tokendispenser/TokenAc2dmResource.java
+++ src/main/java/com/github/yeriomin/tokendispenser/TokenAc2dmResource.java
@@ -2,6 +2,7 @@ package com.github.yeriomin.tokendispenser;
 
 import com.github.yeriomin.playstoreapi.GooglePlayAPI;
 import com.github.yeriomin.playstoreapi.GooglePlayException;
+import com.github.yeriomin.playstoreapi.PlayStoreApiBuilder;
 import com.github.yeriomin.playstoreapi.PropertiesDeviceInfoProvider;
 import spark.HaltException;
 import spark.Request;
@@ -11,15 +12,14 @@ import spark.Route;
 import java.io.IOException;
 import java.util.Locale;
 import java.util.Properties;
+import java.util.logging.Logger;
 
 import static spark.Spark.halt;
 
 public class TokenAc2dmResource {
 
-    private String email;
-
     public String handle(Request request, Response response) {
-        email = request.params("email");
+        String email = request.params("email");
         String password = Server.passwords.get(email);
         if (null == password || password.isEmpty()) {
             halt(404, "No password for this email");
@@ -27,15 +27,17 @@ public class TokenAc2dmResource {
         int code = 500;
         String message;
         try {
-            return getToken(password);
+            return getToken(email, password);
         } catch (GooglePlayException e) {
             if (e.getCode() >= 400) {
                 code = e.getCode();
             }
             message = e.getMessage();
+            Logger.getGlobal().warning(e.getClass().getName() + ": " + message);
             halt(code, message);
         } catch (IOException e) {
             message = e.getMessage();
+            Logger.getGlobal().warning(e.getClass().getName() + ": " + message);
             halt(code, message);
         }
         return "";
@@ -53,14 +55,15 @@ public class TokenAc2dmResource {
         deviceInfoProvider.setProperties(properties);
         deviceInfoProvider.setLocaleString(Locale.ENGLISH.toString());
 
-        GooglePlayAPI api = new GooglePlayAPI(email);
+        GooglePlayAPI api = new GooglePlayAPI();
+        api.setClient(new OkHttpClientAdapter());
         api.setDeviceInfoProvider(deviceInfoProvider);
         api.setLocale(Locale.US);
         return api;
     }
 
-    protected String getToken(String password) throws IOException {
-        return getApi().getAC2DMToken(password);
+    protected String getToken(String email, String password) throws IOException {
+        return getApi().generateAC2DMToken(email, password);
     }
 
 }
--- src/main/java/com/github/yeriomin/tokendispenser/TokenResource.java
+++ src/main/java/com/github/yeriomin/tokendispenser/TokenResource.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 public class TokenResource extends TokenAc2dmResource {
 
     @Override
-    protected String getToken(String password) throws IOException {
-        return getApi().getToken(password);
+    protected String getToken(String email, String password) throws IOException {
+        return getApi().generateToken(email, password);
     }
 }
